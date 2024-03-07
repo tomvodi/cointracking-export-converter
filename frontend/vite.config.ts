35dc5eda0f549c4d8674e00009b2e69adcb690de
Tom <tomvodi@protonmail.com>
2024-03-07 12:47:27 +0000

    added testing libraries

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment:"jsdom",
+    server: {
+      deps: {
+        inline: ['vuetify']
+      }
+    }
+  }
 })
