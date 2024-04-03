9b3f44864c317b8c738c276b087707794c057779
Tom <tomvodi@protonmail.com>
2024-04-03 14:29:23 +0200

    fixed AddExportFile component test

--- frontend/src/tests/components/AddExportFile.test.ts
+++ frontend/src/tests/components/AddExportFile.test.ts
@@ -1,4 +1,4 @@
-import {describe, expect, test, vi} from "vitest";
+import {describe, expect, test} from "vitest";
 import {createVuetify} from "vuetify";
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -42,12 +42,11 @@ function defaultWrapper(): VueWrapper {
 
 describe("initial state", () => {
     test("should show notification when no timezone is set initially", async () => {
-        const store = vi.mocked(useSettingsStore(pinia));
-
+        const store = useSettingsStore(pinia);
         // @ts-ignore
         store.timezoneEmpty = true;
-
         const wrapper = defaultWrapper();
+
         expect(wrapper.find('.v-alert').exists()).toBeTruthy();
         const selectFileBtn = wrapper.find('.v-btn');
         expect(selectFileBtn.attributes().disabled).toBeDefined();
@@ -60,6 +59,7 @@ describe("timezone set", () => {
         // @ts-ignore
         store.timezoneEmpty = false;
         const wrapper = defaultWrapper();
+
         expect(wrapper.find('.v-alert').exists()).toBeFalsy();
         const selectFileBtn = wrapper.find('.v-btn');
         expect(selectFileBtn.attributes().disabled).toBeDefined();
